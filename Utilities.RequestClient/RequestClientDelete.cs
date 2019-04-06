331c184ca44f7169d567e4b0cb36f30e2b72e017
Mehmet <mnabaci@gmail.com>
2019-04-06 09:09:33 +0300

    version update nonbuilding code fixed

--- Utilities.RequestClient/RequestClientDelete.cs
+++ Utilities.RequestClient/RequestClientDelete.cs
@@ -26,7 +26,7 @@ namespace Utilities.RequestClient
             try
             {
                 var response = Client.DeleteAsync(GetCompleteUrl(uri)).Result;
-                var responseContent = typeof(T) == typeof(string) ? response.Content.ReadAsStringAsync(new List<MediaTypeFormatter> { MediaTypeFormatter }).Result.Deserialize<T>(SerializationType) :
+                var responseContent = typeof(T) == typeof(string) ? response.Content.ReadAsStringAsync().Result.Deserialize<T>(SerializationType) :
                                                                     response.Content.ReadAsAsync<T>(new List<MediaTypeFormatter> { MediaTypeFormatter }).Result;
                 return new RequestResult<T> { Result = responseContent, StatusCode = response.StatusCode, ExceptionDetail = response.StatusCode == HttpStatusCode.OK ? string.Empty : responseContent.Serialize() };
             }
